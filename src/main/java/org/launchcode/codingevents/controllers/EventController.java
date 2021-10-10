c14156e66327512593db379ab841b8bf28a59474
Matt <dyermcd@gmail.com>
2021-10-09 20:50:25 -0500

    completed class 10 exercises

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,16 +8,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
-    private static List<String> events = new ArrayList<>();
+    //private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap<>();
+
 
     @GetMapping
     public String displayAllEvents(Model model) {
+        //model.addAttribute("events", events);
+        //return "events/index";
         model.addAttribute("events", events);
+        events.put("1 Name","1 Desc");
+        events.put("2 Name","2 Desc");
+        events.put("3 Name","3 Desc");
         return "events/index";
     }
 
@@ -28,8 +37,8 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, String eventDesc) {
+        events.put(eventName, eventDesc);
         return "redirect:";
     }
 
